9f116da22e159f38bfcd4bfa7266bd5f716e3f06
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-05-13 13:52:33 +0900

    🎉 initial commit

---
+++ src/main/java/Proring/Proring_prototype/ProringPrototypeApplication.java
@@ -0,0 +1,12 @@
+package Proring.Proring_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProringPrototypeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProringPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Proring/Proring_prototype/ProringPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package Proring.Proring_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProringPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
